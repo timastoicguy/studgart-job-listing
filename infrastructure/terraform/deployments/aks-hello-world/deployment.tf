bb98418cd542c31e7da234054f654f949620a4e0
Thinh Nguyen <thinh.nguyen@codeleap.de>
2025-04-20 00:32:30 +0700

    change: added terraform code to deploy aks-hello-world

---
+++ infrastructure/terraform/deployments/aks-hello-world/deployment.tf
@@ -0,0 +1,9 @@
+resource "helm_release" "aks_hello_world" {
+  name             = "aks-hello-world"
+  namespace        = "aks-hello-world"
+  create_namespace = true
+  chart            = "../../../helm-charts/aks-hello-world"
+  values = [
+    "${file("${path.module}/values.yml")}"
+  ]
+}
---
+++ infrastructure/terraform/deployments/aks-hello-world/terraform.tfvars
@@ -0,0 +1,2 @@
+aks_name = "joblisting-aks"
+rg_name = "job-listing-rg"
