f3f574c022d9a3f0e9b83601551fa007052b518a
Nguyendungx <dung0916149123@gmail.com>
2025-05-27 21:45:02 +0700

    Xong

--- backend/src/models/job_types.ts
+++ backend/src/models/job_types.ts
@@ -5,6 +5,10 @@ const jobTypeSchema = new Schema({
   name: { type: String, required: true },
   code: { type: String, required: true, unique: true },
   priority: { type: Number, default: 1 },
+
+
+
+  
 });
 
 export const JobType = model("JobType", jobTypeSchema);
